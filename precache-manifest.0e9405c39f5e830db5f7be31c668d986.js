4264336b251e032aa4b2385a9920afa9329098ad
Ajay <user@Ankitas-MacBook-Air.local>
2023-12-24 00:19:36 +0530

    Updates

---
+++ precache-manifest.0e9405c39f5e830db5f7be31c668d986.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b98fe9ac3ec89bdc7a0ae310620724",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0568720634f0c54c495",
+    "url": "/static/css/main.68bbecdd.chunk.css"
+  },
+  {
+    "revision": "f011f8e2bf0e28172e11",
+    "url": "/static/js/2.a0528ecd.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/static/js/2.a0528ecd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0568720634f0c54c495",
+    "url": "/static/js/main.f4caa5a9.chunk.js"
+  },
+  {
+    "revision": "9bc2349bfbe76e87395f",
+    "url": "/static/js/runtime-main.e5afff51.js"
+  }
+]);
